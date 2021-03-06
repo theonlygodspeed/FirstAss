988a254b6553b0bd41a55227b959466e583cc498
theonlygodspeed <godspeedguinto@gmail.com>
2021-03-06 22:41:04 +0800

    rewrite code

--- person.java
+++ person.java
@@ -1,27 +1,3 @@
-public int getGodspeedGuinto()
-{
-  return name;
-}
-public class name {
-    private string name;
-    public string getname() {
-        return name;
-    }
-    public void setname(string newname) {
-        name = name;
-    }
-}
-
-
-public void set jasper(string Age) {
-    this.age = age;
-}
-public class name {
-    private string name;
-    public string getname() {
-        return this.Age;
-    }
-    public void set name(string name) {
-        this.name = name;
-    }
-}
+public class Main{
+  String firstname = "Godspeed";
+  String lastname  = "Guinto";
