b1b6a122591f104fced72d105663eecd67b3329e
theonlygodspeed <godspeedguinto@gmail.com>
2021-03-06 22:43:47 +0800

    Modify the Person.java by updating is public attributes to private and creating accessors and mutators for the given attributes.

--- person.java
+++ person.java
@@ -1,3 +1,27 @@
-public class Main{
-  String firstname = "Godspeed";
-  String lastname  = "Guinto";
+public int getGuinto()
+{
+  return name;
+}
+public class name {
+    private string name;
+    public string getname() {
+        return name;
+    }
+    public void setname(string newname) {
+        name = name;
+    }
+}
+
+
+public void set jasper(string Age) {
+    this.age = age;
+}
+public class name {
+    private string name;
+    public string getname() {
+        return this.Age;
+    }
+    public void set name(string name) {
+        this.name = name;
+    }
+}
